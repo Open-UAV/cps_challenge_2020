881b692fc108fe998c3165182bb5cfd53b2d507f
Jnaneshwar "JD" Das <jdas5@asu.edu>
2020-05-29 10:13:31 -0700

    overwrite iris.sdf in Firmware source tree

--- cps_phase2.sh
+++ cps_phase2.sh
@@ -2,6 +2,5 @@
 git clone https://github.com/Open-UAV/cps_challenge_2020
 rsync -aP cps_challenge_2020/ /root/catkin_ws/src/cps_challenge_2020/
 wget https://download.openuas.us/cps_challenge_2020/models/iris/iris.sdf -O /root/catkin_ws/src/cps_challenge_2020/models/iris/iris.sdf
-cp /root/catkin_ws/src/cps_challenge_2020/models/iris/iris.sdf /root/src/Firmware/Tools/sitl_gazebo/models/iris/iris.sdf
+cp -rf /root/catkin_ws/src/cps_challenge_2020/models/iris/iris.sdf /root/src/Firmware/Tools/sitl_gazebo/models/iris/iris.sdf
 apt-get -y install xmlstarlet
-
